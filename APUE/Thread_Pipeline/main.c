145228930834bcdc88ad9380128cc714aaf8e92d
xzmjx2 <2013257123@qq.com>
2021-10-27 19:26:18 +0800

    修改笔记

--- APUE/Thread_Pipeline/main.c
+++ APUE/Thread_Pipeline/main.c
@@ -3,13 +3,28 @@
 #include "stdio.h"
 #include <pthread.h>
 
+static void* read_handle(void* p)
+{
+    
+}
 
+static void* write_handle(void* p)
+{
+
+
+}
 static mypipe_t* p_MyPipe;
 int main()
 {
     p_MyPipe = mypipe_init();
 
     pthread_t tid_rd,tid_wr;
-    pthread_create()
+    pthread_create(&tid_rd,NULL,read_handle,NULL);
+    pthread_create(&tid_wr,NULL,write_handle,NULL);
 
+    void* ptr;
+    pthread_join(tid_wr,ptr);
+    pthread_join(tid_rd,ptr);
+
+    mypipe_destory(p_MyPipe);
 }
