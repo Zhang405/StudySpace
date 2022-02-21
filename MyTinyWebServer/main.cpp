3a97d45073587e9d364acbb7d2e1f4fa986f7f3a
XZMJX <2013257123@qq.com>
2022-02-21 16:49:59 +0800

    Webserver

--- MyTinyWebServer/main.cpp
+++ MyTinyWebServer/main.cpp
@@ -6,8 +6,8 @@ int main(int argc, char *argv[])
 {
     //需要修改的数据库信息,登录名,密码,库名
     string user = "root";
-    string passwd = "root";
-    string databasename = "qgydb";
+    string passwd = "XZMJX";
+    string databasename = "zmj";
 
     //命令行解析
     Config config;
